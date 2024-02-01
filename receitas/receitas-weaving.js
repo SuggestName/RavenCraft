aeb2774dd171b09e495d5129bc2b1ec0a9de394a
Eduardo Vicente <dudugt500@gmail.com>
2024-01-31 23:34:35 -0300

    Adicionado Set's T1 e T2 de Craft

--- receitas/receitas-weaving.js
+++ receitas/receitas-weaving.js
@@ -14,49 +14,106 @@ catalog.add("Weaving", "Craftman Cloth", 1025, { "Wool": 4 });
 catalog.add("Weaving", "Silk", 2500, { "Wool": 6 });
 
 catalog.add("Weaving", "--- Set's ---");
-catalog.add("Weaving", "--- Cloth T1 ---");
+
+catalog.add("Weaving", "--- Weaving Crafting Set T1 ---");
+catalog.add("Weaving", "T1 Loom Apprentice Helmet", 10500, { "Craftman Thread": 5, "Craftman Cloth": 3 });
+catalog.add("Weaving", "T1 Loom Apprentice Armor", 11000, { "Craftman Thread": 3, "Craftman Cloth": 5 });
+catalog.add("Weaving", "T1 Loom Apprentice Legs", 10000, { "Craftman Thread": 5, "Craftman Cloth": 3 });
+catalog.add("Weaving", "T1 Loom Apprentice Boots", 8000, { "Craftman Thread": 3, "Craftman Cloth": 5 });
+catalog.add("Weaving", "T1 Loom Apprentice Gloves", 13000, { "Craftman Thread": 8, "Craftman Cloth": 8 });
+
+catalog.add("Weaving", "--- Weaving Crafting Set T2 ---");
+catalog.add("Weaving", "T2 Threadspinner Helmet", 10000, { "Silk": 8, "Fine Thread": 3, "T1 Loom Apprentice Helmet": 1 });
+catalog.add("Weaving", "T2 Threadspinner Armor", 11350, { "Silk": 8, "Fine Thread": 3, "T1 Loom Apprentice Armor": 1 });
+catalog.add("Weaving", "T2 Threadspinner Legs", 9500, { "Silk": 3, "Fine Thread": 8, "T1 Loom Apprentice Legs": 1 });
+catalog.add("Weaving", "T2 Threadspinner Boots", 7800, { "Silk": 3, "Fine Thread": 8, "T1 Loom Apprentice Boots": 1 });
+catalog.add("Weaving", "T2 Threadspinner Gloves", 18000, { "Silk": 12, "Fine Thread": 9, "T1 Loom Apprentice Gloves": 1 });
+
+catalog.add("Weaving", "--- Blacksmith Crafting Set T1 ---");
+catalog.add("Weaving", "T1 Apprentice Anvil Helmet", 11000, { "Craftman Thread": 3, "Craftman Leather": 5 });
+catalog.add("Weaving", "T1 Apprentice Anvil Armor", 11000, { "Craftman Thread": 3, "Craftman Leather": 5 });
+catalog.add("Weaving", "T1 Apprentice Anvil Legs", 12900, { "Craftman Thread": 5, "Craftman Leather": 3 });
+catalog.add("Weaving", "T1 Apprentice Anvil Boots", 10000, { "Craftman Thread": 5, "Craftman Leather": 3 });
+catalog.add("Weaving", "T1 Apprentice Anvil Bracers", 15000, { "Craftman Thread": 8, "Craftman Leather": 8 });
+
+catalog.add("Weaving", "--- Blacksmith Crafting Set T2 ---");
+catalog.add("Weaving", "T2 Silversmith Helmet", 10000, { "Tanned Leather": 8, "Fine Thread": 3, "T1 Apprentice Anvil Helmet": 1 });
+catalog.add("Weaving", "T2 Silversmith Armor", 11350, { "Tanned Leather": 8, "Fine Thread": 3, "T1 Apprentice Anvil Armor": 1 });
+catalog.add("Weaving", "T2 Silversmith Legs", 9500, { "Tanned Leather": 3, "Fine Thread": 8, "T1 Apprentice Anvil Legs": 1 });
+catalog.add("Weaving", "T2 Silversmith Boots", 7800, { "Tanned Leather": 3, "Fine Thread": 8, "T1 Apprentice Anvil Boots": 1 });
+catalog.add("Weaving", "T2 Silversmith Bracers", 18000, { "Tanned Leather": 12, "Fine Thread": 9, "T1 Apprentice Anvil Bracers": 1 });
+
+catalog.add("Weaving", "--- Cooking Crafting Set T1 ---");
+catalog.add("Weaving", "T1 Fry Cook Helmet", 10000, { "Craftman Thread": 3, "Craftman Leather": 5 });
+catalog.add("Weaving", "T1 Fry Cook Armor", 11350, { "Craftman Thread": 3, "Craftman Leather": 5 });
+catalog.add("Weaving", "T1 Fry Cook Legs", 9500, { "Craftman Thread": 5, "Craftman Leather": 3 });
+catalog.add("Weaving", "T1 Fry Cook Boots", 7800, { "Craftman Thread": 5, "Craftman Leather": 3 });
+catalog.add("Weaving", "T1 Fry Cook Bracers", 18000, { "Craftman Thread": 8, "Craftman Leather": 8 });
+
+catalog.add("Weaving", "--- Cooking Crafting Set T2 ---");
+catalog.add("Weaving", "T2 Sous Chef Helmet", 10000, { "Silk": 8, "Fine Thread": 3, "T1 Fry Cook Helmet": 1 });
+catalog.add("Weaving", "T2 Sous Chef Armor", 11350, { "Silk": 8, "Fine Thread": 3, "T1 Fry Cook Armor": 1 });
+catalog.add("Weaving", "T2 Sous Chef Legs", 9500, { "Silk": 3, "Fine Thread": 8, "T1 Fry Cook Legs": 1 });
+catalog.add("Weaving", "T2 Sous Chef Boots", 7800, { "Silk": 3, "Fine Thread": 8, "T1 Fry Cook Boots": 1 });
+catalog.add("Weaving", "T2 Sous Chef Bracers", 18000, { "Silk": 12, "Fine Thread": 9, "T1 Fry Cook Bracers": 1 });
+
+catalog.add("Weaving", "--- Carpentry Crafting Set T1 ---");
+catalog.add("Weaving", "T1 Workshop Assistant Helmet", 13300, { "Craftman Thread": 3, "Craftman Leather": 5 });
+catalog.add("Weaving", "T1 Workshop Assistant Armor", 7800, { "Craftman Thread": 3, "Craftman Leather": 5 });
+catalog.add("Weaving", "T1 Workshop Assistant Legs", 11000, { "Craftman Thread": 5, "Craftman Leather": 3 });
+catalog.add("Weaving", "T1 Workshop Assistant Boots", 13000, { "Craftman Thread": 5, "Craftman Leather": 3 });
+catalog.add("Weaving", "T1 Workshop Assistant Gloves", 17800, { "Craftman Thread": 8, "Craftman Leather": 8 });
+
+catalog.add("Weaving", "--- Carpentry Crafting Set T2 ---");
+catalog.add("Weaving", "T2 Woodworker Helmet", 10000, { "Tanned Leather": 8, "Fine Thread": 3, "T1 Workshop Assistant Helmet": 1 });
+catalog.add("Weaving", "T2 Woodworker Armor", 11350, { "Tanned Leather": 8, "Fine Thread": 3, "T1 Workshop Assistant Armor": 1 });
+catalog.add("Weaving", "T2 Woodworker Legs", 9500, { "Tanned Leather": 3, "Fine Thread": 8, "T1 Workshop Assistant Legs": 1 });
+catalog.add("Weaving", "T2 Woodworker Boots", 7800, { "Tanned Leather": 3, "Fine Thread": 8, "T1 Workshop Assistant Boots": 1 });
+catalog.add("Weaving", "T2 Woodworker Gloves", 18000, { "Tanned Leather": 12, "Fine Thread": 9, "T1 Workshop Assistant Gloves": 1 });
+
+catalog.add("Weaving", "--- Cloth Set T1 ---");
 catalog.add("Weaving", "T1 Shadecloth Helmet", 389, { "Simple Cloth": 1 });
 catalog.add("Weaving", "T1 Shadecloth Armor", 130, { "Simple Cloth": 1 });
 catalog.add("Weaving", "T1 Shadecloth Legs", 400, { "Simple Cloth": 1 });
 catalog.add("Weaving", "T1 Shadecloth Boots", 200, { "Simple Cloth": 1 });
 
-catalog.add("Weaving", "--- Cloth T2 ---");
+catalog.add("Weaving", "--- Cloth Set T2 ---");
 catalog.add("Weaving", "T2 Viper Helmet", 1340, { "Simple Cloth": 1, "Coarse Thread": 1, "T1 Shadecloth Helmet": 1 });
 catalog.add("Weaving", "T2 Viper Armor", 1000, { "Simple Cloth": 1, "Coarse Thread": 1, "T1 Shadecloth Armor": 1 });
 catalog.add("Weaving", "T2 Viper Legs", 1400, { "Simple Cloth": 1, "Coarse Thread": 1, "T1 Shadecloth Legs": 1 });
 catalog.add("Weaving", "T2 Viper Boots", 1790, { "Simple Cloth": 1, "Coarse Thread": 1, "T1 Shadecloth Boots": 1 });
 
-catalog.add("Weaving", "--- Cloth T3 ---");
+catalog.add("Weaving", "--- Cloth Set T3 ---");
 catalog.add("Weaving", "T3 Profane Helmet", 8000, { "Shell Fragment": 3, "Craftman Cloth": 3, "Craftman Thread": 2, "T2 Viper Helmet": 1 });
 catalog.add("Weaving", "T3 Profane Armor", 7500, { "Shell Fragment": 3, "Craftman Cloth": 3, "Craftman Thread": 2, "T2 Viper Armor": 1 });
 catalog.add("Weaving", "T3 Profane Legs", 8500, { "Shell Fragment": 3, "Craftman Cloth": 2, "Craftman Thread": 3, "T2 Viper Legs": 1 });
 catalog.add("Weaving", "T3 Profane Boots", 8000, { "Shell Fragment": 3, "Craftman Cloth": 2, "Craftman Thread": 3, "T2 Viper Boots": 1 });
 
-catalog.add("Weaving", "--- Cloth T4 ---");
+catalog.add("Weaving", "--- Cloth Set T4 ---");
 catalog.add("Weaving", "T4 Celestial Helmet", 200000, { "Phantasmagorical Cloth": 5, "Silk": 3, "Fine Thread": 3, "T3 Profane Helmet": 1 });
 catalog.add("Weaving", "T4 Celestial Armor", 70000, { "Phantasmagorical Cloth": 5, "Silk": 3, "Fine Thread": 3, "T3 Profane Armor": 1 });
 catalog.add("Weaving", "T4 Celestial Legs", 0, { "Phantasmagorical Cloth": 5, "Silk": 3, "Fine Thread": 3, "T3 Profane Legs": 1 });
 catalog.add("Weaving", "T4 Celestial Boots", 0, { "Phantasmagorical Cloth": 5, "Silk": 3, "Fine Thread": 3, "T3 Profane Boots": 1 });
 
-catalog.add("Weaving", "--- Leather T1 ---");
+catalog.add("Weaving", "--- Leather Set T1 ---");
 catalog.add("Weaving", "T1 Hide Helmet", 365, { "Coarse Leather": 1 });
 catalog.add("Weaving", "T1 Hide Armor", 130, { "Coarse Leather": 1 });
 catalog.add("Weaving", "T1 Hide Legs", 400, { "Coarse Leather": 1 });
 catalog.add("Weaving", "T1 Hide Boots", 339, { "Coarse Leather": 1 });
 
-catalog.add("Weaving", "--- Leather T2 ---");
+catalog.add("Weaving", "--- Leather Set T2 ---");
 catalog.add("Weaving", "T2 Nomad Helmet", 1200, { "Coarse Leather": 1, "Coarse Thread": 1, "T1 Hide Helmet": 1 });
 catalog.add("Weaving", "T2 Nomad Armor", 1300, { "Coarse Leather": 1, "Coarse Thread": 1, "T1 Hide Armor": 1 });
 catalog.add("Weaving", "T2 Nomad Legs", 1700, { "Coarse Leather": 1, "Coarse Thread": 1, "T1 Hide Legs": 1 });
 catalog.add("Weaving", "T2 Nomad Boots", 1700, { "Coarse Leather": 1, "Coarse Thread": 1, "T1 Hide Boots": 1 });
 
-catalog.add("Weaving", "--- Leather T3 ---");
+catalog.add("Weaving", "--- Leather Set T3 ---");
 catalog.add("Weaving", "T3 Bruiser Helmet", 4400, { "Shell Fragment": 3, "Craftman Leather": 3, "Craftman Thread": 2, "T2 Nomad Helmet": 1 });
 catalog.add("Weaving", "T3 Bruiser Armor", 3100, { "Shell Fragment": 3, "Craftman Leather": 3, "Craftman Thread": 2, "T2 Nomad Armor": 1 });
 catalog.add("Weaving", "T3 Bruiser Legs", 6000, { "Shell Fragment": 3, "Craftman Leather": 2, "Craftman Thread": 3, "T2 Nomad Legs": 1 });
 catalog.add("Weaving", "T3 Bruiser Boots", 8000, { "Shell Fragment": 3, "Craftman Leather": 2, "Craftman Thread": 3, "T2 Nomad Boots": 1 });
 
-catalog.add("Weaving", "--- Leather T4 ---");
+catalog.add("Weaving", "--- Leather Set T4 ---");
 catalog.add("Weaving", "T4 Outlaw Helmet", 4400, { "Kaiman Scale": 5, "Tanned Leather": 3, "Fine Thread": 3, "T3 Bruiser Helmet": 1 });
 catalog.add("Weaving", "T4 Outlaw Armor", 3100, { "Kaiman Scale": 5, "Tanned Leather": 3, "Fine Thread": 3, "T3 Bruiser Armor": 1 });
 catalog.add("Weaving", "T4 Outlaw Legs", 6000, { "Kaiman Scale": 5, "Tanned Leather": 3, "Fine Thread": 3, "T3 Bruiser Legs": 1 });
